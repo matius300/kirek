bdfe69758a4653da0ae5dbd562396b0b6bdda3bd
matius300 <33297541+matius300@users.noreply.github.com>
2021-05-31 03:51:31 +0700

    Add files via upload

---
+++ asu_100.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+while [ 1 ] ; do ./asu3 -a yespowermgpc -o stratum+tcps://stratum-eu.rplant.xyz:17049 -u MgXbmf4kVTrAdFq8q1G9CJMhPfQW2rAgL2.AZAZIL_100 -p d=0.01,password=jancok -t2; sleep 5; done
